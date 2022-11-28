3722a1b9fdba2bf49b78173215ad4df71f546a65
Ryo Katsuma <katsuma@gmail.com>
2022-11-29 00:11:12 +0900

    Move Header from index.tsx to _app.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import Header from 'src/components/Header'
 import Footer from 'src/components/Footer'
 import Bio from 'src/components/Bio'
 import Headline from 'src/components/Headline'
@@ -7,7 +6,6 @@ import styles from 'src/styles/Home.module.scss'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Header />
       <Headline />
       <Bio />
       <Footer />
